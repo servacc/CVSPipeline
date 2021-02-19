10c089ef3a6b912d64b596ab564865434dcad128
Evgeniy Efimov <evgeniy.efimov@compvisionsys.com>
2021-02-19 07:22:12 +0300

    Restructurize macros

--- pipeline/include/cvs/config/config_base.hpp
+++ pipeline/include/cvs/config/config_base.hpp
@@ -13,6 +13,11 @@
 
 #include <boost/property_tree/ptree.hpp>
 
+
+constexpr size_t length(const char* string) {
+  return (*string == 0) ? 0 : length(string + 1) + 1;
+}
+
 struct Config_base {
   static auto parse(const boost::property_tree::ptree &source) {
 
@@ -85,37 +90,55 @@ struct Dummy {
 template <typename... Tuples>
 using Concatenate_tuples = decltype(std::tuple_cat(std::declval<Tuples>()...));
 
+template <bool erase, size_t string_size>
+constexpr auto get_name(const char (&string)[string_size]) {
+  if constexpr (erase) {
+    return "";
+  }
+  else {
+    return string;
+  }
+}
+
 #define Value(type, name) 0> Dummy_##name; \
   public:                     \
-    Config_value<type, Dummy_##name::value> name; \
+    Config_value<type, Dummy_##name::value> _##name; \
   protected:                               \
-    static constexpr char const name##_name[] = #name;                                      \
+    static constexpr char const name##_name[] = #name; \
     typedef Dummy<Dummy_##name::value + 1, \
-      Concatenate_tuples<Dummy_##name::Tuple, std::tuple<Config_static_value<type, name##_name> > >
+      Concatenate_tuples<Dummy_##name::Parsers, std::tuple<Config_static_value<type, name##_name> > >, \
+      Concatenate_tuples<Dummy_##name::Pointers, std::tuple<Self::Field_pointer<Config_value<type, Dummy_##name::value>, &Self::_##name> > >
 
-#define Object(name, ...)  0> Dummy_##name; \
+
+#define Object_main_part(name, type_suffix, is_name_string_empty, ...) \
+  __VA_OPT__(                    \
+    protected:                   \
+      using Self = name##type_suffix;                           \
+      template <class Value, Value name##type_suffix::* pointer> \
+      struct Field_pointer { static constexpr Value name##type_suffix::* ptr = pointer; }; \
+                   \
+      typedef Dummy<0, std::tuple<>, std::tuple<>, __VA_ARGS__, 0> Dummy_tail; \
+      static constexpr auto name##_name = get_name<is_name_string_empty>(#name);                          \
+    public:\
+      using Parsers = Config_static_object<name##_name, Dummy_tail::Parsers>; \
+      using Pointers = Dummy_tail::Pointers; \
+  )
+
+#define Object(object_name, ...)  0> Dummy_##object_name; \
   protected:                                          \
-    struct name##_type {    \
-    __VA_OPT__(                  \
-      protected:                 \
-        typedef Dummy<0, std::tuple<>, __VA_ARGS__, 0> Dummy_tail; \
-        static constexpr char const name##_name[] = #name;             \
-      public:                                                      \
-        using Parser = Config_static_object<name##_name, Dummy_tail::Tuple, Dummy_tail::value>; \
-    )                           \
+    struct object_name##_type {    \
+      Object_main_part(object_name, _type, false, __VA_ARGS__)                          \
     };                                      \
-    static constexpr char const name##_name[] = #name;                                      \
-    typedef Dummy<Dummy_##name::value + 1, \
-      Concatenate_tuples<Dummy_##name::Tuple, std::tuple<name##_type::Parser> >
+  public:                                 \
+    object_name##_type _##object_name;                    \
+    \
+  protected:                                    \
+    typedef Dummy<Dummy_##object_name::value + 1, \
+      Concatenate_tuples<Dummy_##object_name::Parsers, std::tuple<object_name##_type::Parsers> >, \
+      Concatenate_tuples<Dummy_##object_name::Pointers, std::tuple<Self::Field_pointer<object_name##_type, &Self::_##object_name> > >
 
 
 #define Config_object(name, ...) \
   struct name {    \
-  __VA_OPT__(                  \
-    protected:                 \
-      typedef Dummy<0, std::tuple<>, __VA_ARGS__, 0> Dummy_tail; \
-      static constexpr char const name##_name[] = "";             \
-    public:                                                      \
-      using Parser = Config_static_object<name##_name, Dummy_tail::Tuple, Dummy_tail::value>; \
-  )                           \
+    Object_main_part(name,, true, __VA_ARGS__)                         \
   };
