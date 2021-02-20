61ef81c7736ebaebe76dbc215029642a5843b2d3
Evgeniy Efimov <evgeniy.efimov@compvisionsys.com>
2021-02-20 07:03:38 +0300

    Fix macros and templates (now it's working)

--- pipeline/include/cvs/config/config_base.hpp
+++ pipeline/include/cvs/config/config_base.hpp
@@ -18,9 +18,46 @@ constexpr size_t length(const char* string) {
   return (*string == 0) ? 0 : length(string + 1) + 1;
 }
 
-struct Config_base {
-  static auto parse(const boost::property_tree::ptree &source) {
+class Config_base {
+  template <class Pointers, size_t index, class Object_type, class Data>
+  static constexpr void helper(Object_type& object, Data&& data) {
+    object.*std::tuple_element<index, Pointers>::type::ptr =
+        typename std::tuple_element<index, Pointers>::type::Value_type(std::get<index>(std::forward<Data>(data)));
+  }
 
+  template <class Pointers, class Object_type, class Data, std::size_t... indexes>
+  static constexpr void make_from_tuple_impl(Object_type& object, Data&& data, std::index_sequence<indexes...>) {
+    return (helper<Pointers, indexes>(object, data), ...);
+  }
+
+ public:
+
+  template <class Pointers, class Object_type, class Data>
+  static constexpr void make_from_tuple_aggregate(Object_type& object, Data&& data) {
+    return make_from_tuple_impl<Pointers>(
+        object,
+        std::forward<Data>(data),
+        std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Data>>>{}
+    );
+  }
+
+
+ protected:
+
+  template <class Parser>
+  static auto parse(const boost::property_tree::ptree &source) {
+    return Parser::parse(source);
+  }
+
+ public:
+  template <class Result>
+  static std::optional<Result> make(const boost::property_tree::ptree &source) {
+    auto data = parse<typename Result::Parsers>(source);
+    if (!data) {
+      return std::nullopt;
+    }
+
+    return Result(std::move(data.value()));
   }
 };
 
@@ -72,7 +109,6 @@ struct Config_static_object {
   }
 };
 
-// TODO: fix (use name)
 template <class Subtype, auto &name>
 struct Config_static_value {
   static std::optional<Subtype> parse(const boost::property_tree::ptree &source) {
@@ -86,20 +122,9 @@ struct Config_static_value {
   }
 };
 
-template <class Subtype, size_t index>
-class Config_value {
-  Subtype _value;
-
-  // static std::optional<
-
-  operator const Subtype &() const {
-    return _value;
-  }
-};
-
-template <size_t dummy_head, class Parsers_tuple, class Fields_pointers_tuple, size_t dummy_tail = 0>
+template <class Parent_type, class Parsers_tuple, class Fields_pointers_tuple, size_t dummy_tail = 0>
 struct Dummy {
-  static constexpr size_t value = dummy_head;
+  using Parent = Parent_type;
   using Parsers = Parsers_tuple;
   using Pointers = Fields_pointers_tuple;
 };
@@ -119,43 +144,57 @@ constexpr auto get_name(const char (&string)[string_size]) {
 
 #define Value(type, name) 0> Dummy_##name; \
   public:                     \
-    Config_value<type, Dummy_##name::value> _##name; \
+    type _##name; \
   protected:                               \
     static constexpr char const name##_name[] = #name; \
-    typedef Dummy<Dummy_##name::value + 1, \
+    typedef Dummy<Dummy_##name::Parent, \
       Concatenate_tuples<Dummy_##name::Parsers, std::tuple<Config_static_value<type, name##_name> > >, \
-      Concatenate_tuples<Dummy_##name::Pointers, std::tuple<Self::Field_pointer<Config_value<type, Dummy_##name::value>, &Self::_##name> > >
+      Concatenate_tuples<Dummy_##name::Pointers, std::tuple<Self::Field_pointer<type, &Self::_##name> > >
 
 
 #define Object_main_part(name, type_suffix, is_name_string_empty, ...) \
-  __VA_OPT__(                    \
-    protected:                   \
+  __VA_OPT__(                                                          \
+      friend Dummy_##name ::Parent; \
+      friend Config_base;                                              \
+    protected:                                                         \
+        \
       using Self = name##type_suffix;                           \
       template <class Value, Value name##type_suffix::* pointer> \
-      struct Field_pointer { static constexpr Value name##type_suffix::* ptr = pointer; }; \
+      struct Field_pointer { using Value_type = Value; static constexpr Value name##type_suffix::* ptr = pointer; }; \
                    \
-      typedef Dummy<0, std::tuple<>, std::tuple<>, __VA_ARGS__, 0> Dummy_tail; \
+      typedef Dummy<name##type_suffix, std::tuple<>, std::tuple<>, __VA_ARGS__, 0> Dummy_tail; \
       static constexpr auto name##_name = get_name<is_name_string_empty>(#name);                          \
-    public:\
-      using Parsers = Config_static_object<name##_name, Dummy_tail::Parsers>; \
-      using Pointers = Dummy_tail::Pointers; \
+      name##type_suffix() = default;                                                       \
+                                                                                    \
+      using Parsers = Config_static_object<name##_name, Dummy_tail::Parsers>;              \
+      using Pointers = Dummy_tail::Pointers;                           \
+    public:                                                            \
+      template <class Tuple>                                                               \
+      name##type_suffix(Tuple arguments) {                                                \
+        Config_base::make_from_tuple_aggregate<Pointers>(*this, arguments);     \
+      } \
   )
 
 #define Object(object_name, ...)  0> Dummy_##object_name; \
   protected:                                          \
-    struct object_name##_type {    \
-      Object_main_part(object_name, _type, false, __VA_ARGS__)                          \
+    struct object_name##_type {                           \
+      Object_main_part(object_name, _type, false, __VA_ARGS__) \
     };                                      \
   public:                                 \
     object_name##_type _##object_name;                    \
     \
   protected:                                    \
-    typedef Dummy<Dummy_##object_name::value + 1, \
+    typedef Dummy<Self, \
       Concatenate_tuples<Dummy_##object_name::Parsers, std::tuple<object_name##_type::Parsers> >, \
       Concatenate_tuples<Dummy_##object_name::Pointers, std::tuple<Self::Field_pointer<object_name##_type, &Self::_##object_name> > >
 
 
 #define Config_object(name, ...) \
-  struct name {    \
-    Object_main_part(name,, true, __VA_ARGS__)                         \
+  class name {                  \
+    using Dummy_##name = Dummy<name, void, void, 0>; \
+    Object_main_part(name,, true, __VA_ARGS__) \
+   public:\
+    static const std::optional<name> parse_and_make(const boost::property_tree::ptree &source) {   \
+      return Config_base::make<name> (source);       \
+    }                                                                                        \
   };
