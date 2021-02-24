94600379395bb20fca741b7b27889d82ba70325c
Evgeniy Efimov <evgeniy.efimov@compvisionsys.com>
2021-02-24 05:27:19 +0300

    Add various methods to Config

--- pipeline/include/cvs/config/config.h
+++ pipeline/include/cvs/config/config.h
@@ -9,14 +9,34 @@ class Config {
 
   template <class Config_parser>
   Config_parser parse() {
-    static_assert(std::is_base_of<Config_base, Config_parser>::value, "Provided class not derived from Config_base class");
-    return Config_parser(_tree);
+    return Config_parser::parse_and_make(_tree);
+  }
+
+  [[nodiscard]] const std::string_view get_name() const;
+
+  [[nodiscard]] std::vector<Config> get_children() const;
+
+  template <class Result_type>
+  [[nodiscard]] std::optional<Result_type> get_value_optional(const std::string& name) const {
+    auto result = _tree.get_value_optional<Result_type>(name);
+    if (result) {
+      return result;
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  template <class Result_type>
+  [[nodiscard]] Result_type get_value_or_default(const std::string& name, Result_type default_value) const {
+    return _tree.get(name, default_value);
   }
 
  private:
-  Config(const std::string &filename);
+  explicit Config(const boost::property_tree::ptree &tree);
+  explicit Config(const boost::property_tree::ptree::value_type &iterator);
 
  private:
   boost::property_tree::ptree _tree;
-
+  std::string _key;
 };
--- pipeline/src/config/config.cpp
+++ pipeline/src/config/config.cpp
@@ -1,1 +1,35 @@
 #include <cvs/config/config.h>
+
+#include <boost/property_tree/json_parser.hpp>
+
+std::optional<Config> Config::make(const std::string &file_name) {
+
+  boost::property_tree::ptree root;
+  try {
+    boost::property_tree::read_json(file_name, root);
+  }
+  catch (const std::runtime_error &error) {
+    // TODO: logs
+    return std::nullopt;
+  }
+
+  return Config(root);
+}
+
+Config::Config(const boost::property_tree::ptree &tree)
+  : _tree(tree)
+{}
+
+Config::Config(const boost::property_tree::ptree::value_type &iterator)
+    : _tree(iterator.second), _key(iterator.first)
+{}
+
+std::vector<Config> Config::get_children() const {
+  return std::vector<Config>(_tree.begin(), _tree.end());
+}
+
+const std::string_view Config::get_name() const {
+  return _key;
+}
+
+
