f05d43059b8afd659a6744a0c082e42d592e4783
Evgeniy Efimov <evgeniy.efimov@compvisionsys.com>
2021-06-15 19:55:26 +0300

    Add static assert error in case of invalid IElement template argument

--- pipeline/include/cvs/pipeline/ielement.hpp
+++ pipeline/include/cvs/pipeline/ielement.hpp
@@ -5,7 +5,10 @@
 namespace cvs::pipeline {
 
 template <typename Signature>
-class IElement;
+class IElement {
+  static_assert(std::is_invocable<Signature>::value, 
+                "IElement template argument must be function type");
+};
 
 template <typename Res, typename... Args>
 class IElement<Res(Args...)> {
@@ -20,9 +23,21 @@ class IElement<Res(Args...)> {
   virtual bool isStopped() const { return true; }
 };
 
+template <typename Signature>
+struct IElementPointerProxy {
+  static_assert(std::is_invocable<Signature>::value, 
+                "IElementPtr (IElementUPtr) template argument must be function type");
+};
+
+template <typename Res, typename... Args>
+struct IElementPointerProxy<Res(Args...)> {
+  using SharedPointer = std::shared_ptr<IElement<Res(Args...)>>;
+  using UniquePointer = std::unique_ptr<IElement<Res(Args...)>>;
+};
+
 template <typename T>
-using IElementPtr = std::shared_ptr<IElement<T>>;
+using IElementPtr = IElementPointerProxy<T>::SharedPointer;
 template <typename T>
-using IElementUPtr = std::unique_ptr<IElement<T>>;
+using IElementUPtr = IElementPointerProxy<T>::UniquePointer;
 
 }  // namespace cvs::pipeline
